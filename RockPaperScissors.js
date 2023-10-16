44d2d1b99195280b79ffdd24889a13d5b41246bb
Mohammed Aamir Mansoori <72152098+aamirGPT@users.noreply.github.com>
2023-10-17 05:22:19 +0530

    RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,47 +1,50 @@
 //const prompt = require("prompt-sync")({ sigint: true });
 
-//! Creating a rock, paper, scissors game in JavaScript using BOM.
+//! Creating a ROCK, PAPER, SCISSORS game in JavaScript using BOM.
 
-let array = ["Rock", "Paper", "Scissors"];
+let array = ["ROCK", "PAPER", "SCISSORS"];
 let flag = true;
 let player = 0;
 let computer = 0;
 while (flag) {
     const randomElement = array[Math.floor(Math.random() * array.length)];
-    let input = prompt("Rock or Paper or Scissors");
+    let input = prompt("Rock or Paper or Scissors").toUpperCase();
     switch (input) {
-        case "Rock":
-            if (randomElement == "Paper") {
-                alert(`Computer draws: ${randomElement}, Computer +1`);
+        case "ROCK":
+            if (randomElement == "PAPER") {
+                alert(`Computer draws: ${randomElement}!, Computer +1`);
                 computer += 1;
-            } else if (randomElement == "Scissors") {
-                alert(`Computer draws: ${randomElement}, Player +1`);
+            } else if (randomElement == "SCISSORS") {
+                alert(`Computer draws: ${randomElement}!, Player +1`);
                 player += 1;
             } else {
+                alert(`Computer draws: ${randomElement}! as well`);
             }
             break;
-        case "Paper":
-            if (randomElement == "Rock") {
-                alert(`Computer draws: ${randomElement}, Player +1`);
+        case "PAPER":
+            if (randomElement == "ROCK") {
+                alert(`Computer draws: ${randomElement}!, Player +1`);
                 player += 1;
-            } else if (randomElement == "Scissors") {
-                alert(`Computer draws: ${randomElement}, Computer +1`);
+            } else if (randomElement == "SCISSORS") {
+                alert(`Computer draws: ${randomElement}!, Computer +1`);
                 computer += 1;
             } else {
+                alert(`Computer draws: ${randomElement}! as well`);
             }
             break;
-        case "Scissors":
-            if (randomElement == "Paper") {
-                alert(`Computer draws: ${randomElement}, Player +1`);
+        case "SCISSORS":
+            if (randomElement == "PAPER") {
+                alert(`Computer draws: ${randomElement}!, Player +1`);
                 player += 1;
-            } else if (randomElement == "Rock") {
-                alert(`Computer draws: ${randomElement}, Computer +1`);
+            } else if (randomElement == "ROCK") {
+                alert(`Computer draws: ${randomElement}!, Computer +1`);
                 computer += 1;
             } else {
+                alert(`Computer draws: ${randomElement}! as well`);
             }
             break;
         default:
-            console.error("Invalid input, try again.");
+            alert("Enter a valid draw");
             break;
     }
     flag = confirm("Would you like to continue?");
